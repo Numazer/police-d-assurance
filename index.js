5e2afe1438c8857217750f546ec866ccb997ad22
numa <emmanuel.saadisarrault@gmail.com>
2024-10-14 11:44:38 +0200

    avancement

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ function differentstarifs(age, annéespermis, nbAccident, fidélité) {
     let tarif = "refusé";
 }
 
-if(age<25, annéespermis>2, nbAccident=0){
-    tarif= D;
+if(age>25, annéespermis>2, nbAccident=0){
+    tarif= D;}
 else {
     tarif="refusé";
 }    
+
+
+if(age>25, annéespermis<2)(age<25, annéespermis>2) {
+    tarif= C,
+} else {
+    tarif="refusé"
 }
 
-if(age<25, annéespermis<2)
+if(age<25,annéespermis<2)
