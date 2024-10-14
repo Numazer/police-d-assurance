480e198de7bcce675fb71faaa966562ae3369db4
numa <emmanuel.saadisarrault@gmail.com>
2024-10-14 14:43:16 +0200

    html a faire

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function differentstarifs(age, annéespermis, nbAccident, fidélité) {
-    let tarif = "refusé";
-}
+    let tarif ="refusé";
 
-if(age>25, annéespermis>2, nbAccident=0){
-    tarif= D;}
+
+if(age>25 && annéespermis>2 && nbAccident===0){
+    tarif="D";}
 else {
     tarif="refusé";
 }    
 
 
-if(age>25, annéespermis<2)(age<25, annéespermis>2) {
-    tarif= C,
-} else {
-    tarif="refusé"
+if(age>25  && annéespermis<2 || age<25 && annéespermis>2){ 
+    tarif= "C";
+else {
+    tarif="refusé";}
 }
 
-if(age<25,annéespermis<2)
+if(age<25,annéespermis<2){
+    if (nbAccident===0){
+    tarif= "B";}
+    if (nbAccident===1){
+    tarif= "C";}
+    if (nbAccident<2){
+    tarif= "D";}
+    else {
+        tarif="refusé";
+    }
+    }
+
+    if (annéesassu > 1) {
+        if (tarif === 'B') {
+            tarif = 'A';
+        } else if (tarif === 'C') {
+            tarif = 'B';
+        } else if (tarif === 'D') {
+            tarif = 'C';
+        }
+    }
+    return tarif;
+}
+console.log(differentstarifs)
