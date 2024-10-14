6baa9a86e15e9dee833c5bc997aa1e482704f29e
numa <emmanuel.saadisarrault@gmail.com>
2024-10-14 15:05:31 +0200

    ajout formulaire html

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ function differentstarifs(age, annéespermis, nbAccident, fidélité) {
     let tarif ="refusé";
 
 
-if(age>25 && annéespermis>2 && nbAccident===0){
+if (age>25 && annéespermis>2 && nbAccident===0){
     tarif="D";}
 else {
     tarif="refusé";
 }    
 
 
-if(age>25  && annéespermis<2 || age<25 && annéespermis>2){ 
+if ((age>25  && annéespermis<2) || (age<25 && annéespermis>2)) { 
     tarif= "C";
 else {
     tarif="refusé";}
 }
 
-if(age<25,annéespermis<2){
+if (age<25,annéespermis<2){
     if (nbAccident===0){
     tarif= "B";}
     if (nbAccident===1){
