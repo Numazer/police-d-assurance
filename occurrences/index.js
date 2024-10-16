7f07fcf0915ca94e496b7498c9be6f2452745c0c
numa <emmanuel.saadisarrault@gmail.com>
2024-10-16 10:20:16 +0200

    tableau modif

--- occurrences/index.js
+++ occurrences/index.js
@@ -1,9 +1,9 @@
 var indices = [];
-var tableau = ["t"];
-var élément = "x";
+let [t]  = [2,3,1,4,8,2,1,4,5];
+let  x = 1;
 var idx = tableau.indexOf(élément);
 while (idx != -1) {
   indices.push(idx);
   idx = tableau.indexOf(élément, idx + 1);
 }
-console.log(élément);
+console.log(x);
